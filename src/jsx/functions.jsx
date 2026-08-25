c4cb6bf5d376d49cd4934d45684a7ddacc55344a
jOstin-7 <justinreboton98@gmail.com>
2026-08-25 22:55:01 +0800

    added partitions and some navigation

---
+++ src/jsx/functions.jsx
@@ -0,0 +1,25 @@
+
+  const [activeSection, setActiveSection] = useState("about");
+
+  useEffect(() => {
+    const sections = document.querySelectorAll(".partitions");
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveSection(entry.target.id);
+          }
+        });
+      },
+      {
+        threshold: 0.6,
+      }
+    );
+
+    sections.forEach((section) => observer.observe(section));
+
+    return () => {
+      sections.forEach((section) => observer.unobserve(section));
+    };
+  }, []);
